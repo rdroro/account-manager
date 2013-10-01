faba089b0c928e7e82c3faf2279efef7c763ae43
Romain Dubos <romain.dubos@gmail.com>
2013-10-01 12:20:21 +0200

    * Add isAccountOwner Policies * Add view to display Account List 	new file:   api/policies/isAccountOwner.js 	new file:   views/account/accountList.ejs

---
+++ app/api/policies/isAccountOwner.js
@@ -0,0 +1,20 @@
+/**
+ * Check if user in session is owner of :id param account
+ */
+ module.exports = function (req, resp, ok) {
+
+  // The user must be owner of account
+  var accountId = req.param('id');
+  if (!accountId) { return resp.send(404); }
+  var accountList = req.session.user.accounts;
+  // Check if user is the owner of selected account
+  /*User is the owner if account is in the list
+   req.session.user.account/. List is set during signin action*/
+  for (var i = 0; i < accountList.length; i++) {
+   if (accountList[i].id == accountId) {
+    req.param.account = accountList[i];
+    return ok();
+  }
+}
+return resp.send(403, 'You are not the Owner of account');
+};
