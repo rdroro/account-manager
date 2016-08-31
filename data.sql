498a8d9965d000ed4d00349f6af048a550c65ff2
Romain Dubos <romain.dubos@gmail.com>
2016-08-31 07:46:06 +0200

    Enable template of data

--- data.sql
+++ data.sql
@@ -2,6 +2,6 @@
 
 insert into user values ('', '', '', '', 1, NULL, NULL);
 
-insert into account values (1, 'Livret A', 22950, 1, NULL, NULL);
-insert into account values (1, 'BNP - Cheque', 26829.99, 2, NULL, NULL);
-insert into account values (1, 'C. Mutuel - Cheque', 60.51, 3, NULL, NULL);
+insert into account values (1, 'A', 20, 1, NULL, NULL);
+insert into account values (1, 'B', 2.99, 2, NULL, NULL);
+insert into account values (1, 'C', 651, 3, NULL, NULL);
